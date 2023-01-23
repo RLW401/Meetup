f893c94caf0413c9421001f293be911e0c4d8ec0
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-01-23 13:05:58 -0500

    seeder changes

--- backend/db/seeders/20230123051324-demo-membership.js
+++ backend/db/seeders/20230123051324-demo-membership.js
@@ -1,56 +1,56 @@
-// 'use strict';
+'use strict';
 
-// let options = {};
-// if (process.env.NODE_ENV === "production") {
-//   options.schema = process.env.SCHEMA; // define schema in options object
-// }
-// options.tableName = "Memberships";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define schema in options object
+}
+options.tableName = "Memberships";
 
-// const validStatus = ["pending", "waitlist", "member", "co-host"];
+const validStatus = ["pending", "waitlist", "member", "co-host"];
 
-// const membership1 = { groupId: 1, userId: 1, status: validStatus[3]};
-// const membership2 = { groupId: 1, userId: 2, status: validStatus[1]};
-// const membership3 = { groupId: 2, userId: 3, status: validStatus[3]};
-// // const membership3 = { groupId: 1, userId: 3, status: validStatus[3]};
-// const membership4 = { groupId: 3, userId: 3, status: validStatus[3]};
+const membership1 = { groupId: 1, userId: 1, status: validStatus[3]};
+const membership2 = { groupId: 1, userId: 2, status: validStatus[1]};
+const membership3 = { groupId: 2, userId: 3, status: validStatus[3]};
+// const membership3 = { groupId: 1, userId: 3, status: validStatus[3]};
+const membership4 = { groupId: 3, userId: 3, status: validStatus[3]};
 
 
 
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     /**
-//      * Add seed commands here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkInsert('People', [{
-//      *   name: 'John Doe',
-//      *   isBetaMember: false
-//      * }], {});
-//     */
-//     await queryInterface.bulkInsert(options, [
-//       membership1, membership2, membership3, membership4
-//     ], {});
-//   },
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert(options, [
+      membership1, membership2, membership3, membership4
+    ], {});
+  },
 
-//   async down (queryInterface, Sequelize) {
-//     /**
-//      * Add commands to revert seed here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkDelete('People', null, {});
-//      */
-//     const Op = Sequelize.Op;
-//     const where = {
-//       // check on this syntax
-//       [Op.or]: [{
-//         [Op.and]: [{groupId: 1, userId: 1}],
-//         [Op.and]: [{groupId: 1, userId: 2}],
-//         [Op.and]: [{groupId: 2, userId: 3}],
-//         [Op.and]: [{groupId: 3, userId: 3}]
-//       }]
-//     };
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    const where = {
+      // check on this syntax
+      [Op.or]: [{
+        [Op.and]: [{groupId: 1, userId: 1}],
+        [Op.and]: [{groupId: 1, userId: 2}],
+        [Op.and]: [{groupId: 2, userId: 3}],
+        [Op.and]: [{groupId: 3, userId: 3}]
+      }]
+    };
 
-//     await queryInterface.bulkDelete(options, where, {});
-//   }
-// };
+    await queryInterface.bulkDelete(options, where, {});
+  }
+};
--- backend/db/seeders/20230123070934-demo-attendee.js
+++ backend/db/seeders/20230123070934-demo-attendee.js
@@ -1,54 +1,54 @@
-// 'use strict';
+'use strict';
 
-// let options = {};
-// if (process.env.NODE_ENV === "production") {
-//   options.schema = process.env.SCHEMA; // define schema in options object
-// }
-// options.tableName = "Attendees";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define schema in options object
+}
+options.tableName = "Attendees";
 
-// const validStatus = ["pending", "waitlist", "member", "co-host"];
+const validStatus = ["pending", "waitlist", "member", "co-host"];
 
-// const attendee1 = { userId: 1, eventId: 1, status: validStatus[2]};
-// const attendee2 = { userId: 2, eventId: 1, status: validStatus[1]};
-// const attendee3 = { userId: 3, eventId: 2, status: validStatus[2]};
-// const attendee4 = { userId: 3, eventId: 3, status: validStatus[3]};
+const attendee1 = { userId: 1, eventId: 1, status: validStatus[2]};
+const attendee2 = { userId: 2, eventId: 1, status: validStatus[1]};
+const attendee3 = { userId: 3, eventId: 2, status: validStatus[2]};
+const attendee4 = { userId: 3, eventId: 3, status: validStatus[3]};
 
 
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     /**
-//      * Add seed commands here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkInsert('People', [{
-//      *   name: 'John Doe',
-//      *   isBetaMember: false
-//      * }], {});
-//     */
-//     await queryInterface.bulkInsert(options, [
-//       attendee1, attendee2, attendee3, attendee4
-//     ], {});
-//   },
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert(options, [
+      attendee1, attendee2, attendee3, attendee4
+    ], {});
+  },
 
-//   async down (queryInterface, Sequelize) {
-//     /**
-//      * Add commands to revert seed here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkDelete('People', null, {});
-//      */
-//     const Op = Sequelize.Op;
-//     const where = {
-//       // check on this syntax
-//       [Op.or]: [{
-//         [Op.and]: [{userId: 1, eventId: 1}],
-//         [Op.and]: [{userId: 2, eventId: 1}],
-//         [Op.and]: [{userId: 3, eventId: 2}],
-//         [Op.and]: [{userId: 3, eventId: 3}]
-//       }]
-//     };
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    const where = {
+      // check on this syntax
+      [Op.or]: [{
+        [Op.and]: [{userId: 1, eventId: 1}],
+        [Op.and]: [{userId: 2, eventId: 1}],
+        [Op.and]: [{userId: 3, eventId: 2}],
+        [Op.and]: [{userId: 3, eventId: 3}]
+      }]
+    };
 
-//     await queryInterface.bulkDelete(options, where, {});
-//   }
-// };
+    await queryInterface.bulkDelete(options, where, {});
+  }
+};
