47f8e816793081e0880abade527bc7a2cc9964ae
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-01-23 13:02:44 -0500

    seeder changes

--- backend/db/seeders/20230123061539-demo-venue.js
+++ backend/db/seeders/20230123061539-demo-venue.js
@@ -1,68 +1,68 @@
-'use strict';
+// 'use strict';
 
-let options = {};
-if (process.env.NODE_ENV === "production") {
-  options.schema = process.env.SCHEMA; // define schema in options object
-}
-options.tableName = "Venues";
+// let options = {};
+// if (process.env.NODE_ENV === "production") {
+//   options.schema = process.env.SCHEMA; // define schema in options object
+// }
+// options.tableName = "Venues";
 
-const venueAddresses = ["123 Disney Lane", "1 North Pole", "0 South Pole"]
+// const venueAddresses = ["123 Disney Lane", "1 North Pole", "0 South Pole"]
 
-const venue1 =     {
-  "groupId": 1,
-  "address": venueAddresses[0],
-  "city": "New York",
-  "state": "NY",
-  "lat": 37.7645358,
-  "lng": -122.4730327,
-}
+// const venue1 =     {
+//   "groupId": 1,
+//   "address": venueAddresses[0],
+//   "city": "New York",
+//   "state": "NY",
+//   "lat": 37.7645358,
+//   "lng": -122.4730327,
+// }
 
-const venue2 =     {
-  "groupId": 2,
-  "address": venueAddresses[1],
-  "city": "North Pole",
-  "state": "NP",
-  "lat": 90,
-  "lng": 0,
-}
+// const venue2 =     {
+//   "groupId": 2,
+//   "address": venueAddresses[1],
+//   "city": "North Pole",
+//   "state": "NP",
+//   "lat": 90,
+//   "lng": 0,
+// }
 
-const venue3 =     {
-  "groupId": 3,
-  "address": venueAddresses[2],
-  "city": "South Pole",
-  "state": "SP",
-  "lat": -90,
-  "lng": 0,
-}
+// const venue3 =     {
+//   "groupId": 3,
+//   "address": venueAddresses[2],
+//   "city": "South Pole",
+//   "state": "SP",
+//   "lat": -90,
+//   "lng": 0,
+// }
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-   await queryInterface.bulkInsert(options, [
-    venue1, venue2, venue3
-  ], {});
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//    await queryInterface.bulkInsert(options, [
+//     venue1, venue2, venue3
+//   ], {});
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    const Op = Sequelize.Op;
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//     const Op = Sequelize.Op;
 
-    await queryInterface.bulkDelete(options, {
-      address: { [Op.in]: venueAddresses }
-      }, {});
-  }
-};
+//     await queryInterface.bulkDelete(options, {
+//       address: { [Op.in]: venueAddresses }
+//       }, {});
+//   }
+// };
--- backend/db/seeders/20230123073019-demo-image.js
+++ backend/db/seeders/20230123073019-demo-image.js
@@ -1,67 +1,67 @@
-'use strict';
+// 'use strict';
 
-let options = {};
-if (process.env.NODE_ENV === "production") {
-  options.schema = process.env.SCHEMA; // define schema in options object
-}
-options.tableName = "Images";
+// let options = {};
+// if (process.env.NODE_ENV === "production") {
+//   options.schema = process.env.SCHEMA; // define schema in options object
+// }
+// options.tableName = "Images";
 
-const imageURLs = ["./images/tennis", "./images/north-pole", "./images/south-pole.jpg", "./images/sanic-hedgehog.jpg"]
+// const imageURLs = ["./images/tennis", "./images/north-pole", "./images/south-pole.jpg", "./images/sanic-hedgehog.jpg"]
 
-const image1 = {
-  groupId: 1,
-  url: imageURLs[0],
-  groupPreview: true,
-  eventPreview: false
-};
-const image2 = {
-  eventId: 2,
-  url: imageURLs[1],
-  groupPreview: false,
-  eventPreview: true
-};
-const image3 = {
-  eventId: 3,
-  url: imageURLs[2],
-  groupPreview: false,
-  eventPreview: false
-};
-const image4 = {
-  groupId: 3,
-  eventId: 3,
-  url: imageURLs[3],
-  groupPreview: true,
-  eventPreview: true
-};
+// const image1 = {
+//   groupId: 1,
+//   url: imageURLs[0],
+//   groupPreview: true,
+//   eventPreview: false
+// };
+// const image2 = {
+//   eventId: 2,
+//   url: imageURLs[1],
+//   groupPreview: false,
+//   eventPreview: true
+// };
+// const image3 = {
+//   eventId: 3,
+//   url: imageURLs[2],
+//   groupPreview: false,
+//   eventPreview: false
+// };
+// const image4 = {
+//   groupId: 3,
+//   eventId: 3,
+//   url: imageURLs[3],
+//   groupPreview: true,
+//   eventPreview: true
+// };
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-   await queryInterface.bulkInsert(options, [
-    image1, image2, image3, image4
-   ], {});
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//    await queryInterface.bulkInsert(options, [
+//     image1, image2, image3, image4
+//    ], {});
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    const Op = Sequelize.Op;
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//     const Op = Sequelize.Op;
 
-    await queryInterface.bulkDelete(options, {
-      url: { [Op.in]: imageURLs }
-    }, {});
-  }
-};
+//     await queryInterface.bulkDelete(options, {
+//       url: { [Op.in]: imageURLs }
+//     }, {});
+//   }
+// };
