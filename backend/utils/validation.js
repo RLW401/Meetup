931403e2f30e507ca186cfccd3fc18bd78ad6286
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-01-26 11:47:46 -0500

    get all groups route bugfix attempt

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,5 +1,5 @@
 // backend/utils/validation.js
-const { validationResult } = require('express-validator');
+const { validationResult, check } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
@@ -20,4 +20,57 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
-module.exports = { handleValidationErrors };
+const validateGroupBody = [
+  check('name')
+    .exists({ checkFalsy: true })
+    .isLength({ max: 60 })
+    .withMessage("Name must be 60 characters or less"),
+  check('about')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 50 })
+    .withMessage("About must be 50 characters or more"),
+  check('type')
+    .exists({ checkFalsy: true })
+    .matches(/^Online|In person?/) // ?
+    .withMessage("Type must be 'Online' or 'In person'"),
+  check('private')
+    .exists({ checkFalsy: true })
+    .isBoolean()
+    .withMessage("Private must be a boolean"),
+  check('city')
+    .exists({ checkFalsy: true })
+    .withMessage("City is required"),
+  check('state')
+    .exists({ checkFalsy: true })
+    .withMessage("State is required"),
+
+  handleValidationErrors
+];
+
+const validateVenueBody = [
+  check('address')
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check('city')
+    .exists({ checkFalsy: true })
+    .withMessage("City is required"),
+  check('state')
+    .exists({ checkFalsy: true })
+    .withMessage("State is required"),
+  check('lat')
+    .exists({ checkFalsy: true })
+    .isFloat({ min: -90, max: 90 })
+    .withMessage("Latitude is not valid"),
+  check('lng')
+    .exists({ checkFalsy: true })
+    .isFloat({ min: -180, max: 180 })
+    .withMessage("Longitude is not valid"),
+
+    handleValidationErrors
+];
+
+
+
+module.exports = { handleValidationErrors,
+  validateGroupBody,
+  validateVenueBody };
