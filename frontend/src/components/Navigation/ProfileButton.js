5b59994bde6d73cbcf6dcacd00225db91dc388f4
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-02-27 10:58:20 -0500

    done with first stage of navigation

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,9 @@
+const ProfileButton = () => {
+    return (
+      <div style={{ color: "purple", fontSize: "100px" }}>
+        <i class="fa-sharp fa-solid fa-head-side-goggles"></i>
+      </div>
+    );
+};
+
+export default ProfileButton;
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -60,8 +60,7 @@ export const logout = () => async (dispatch) => {
     const response = await csrfFetch("/api/session", {
         method: "DELETE"
     });
-    // const data = await response.json();
-    // if (!("user" in data)) data.user = {...data};
+
     dispatch(removeUser());
 
     return response;
